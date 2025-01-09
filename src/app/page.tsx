e1dfd457f0afed816797c89d7fbdc02ca40364ac
Muhammad Fiqri <muhfiqri033@gmail.com>
2025-01-09 14:00:18 +0800

    solv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,14 @@ import StatsSection from '../components/StatsSection'
 export default function Home() {
   return (
     <>
-
-    </>
-  )
-}
-
-export default page 
-
       <HeroSection />
       <StatsSection />
+
     </>
   )
 }
+
+
+
 
 
