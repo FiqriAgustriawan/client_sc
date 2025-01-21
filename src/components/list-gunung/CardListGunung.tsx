6f6fbf49c276a5b7cc13a57168ec758b326bf3b6
Muhammad Fiqri <muhfiqri033@gmail.com>
2025-01-21 09:42:42 +0800

    done

--- src/components/list-gunung/CardListGunung.tsx
+++ src/components/list-gunung/CardListGunung.tsx
@@ -74,7 +74,7 @@
 import React from "react";
 import Image from "next/image";
 import BuluBaria from "@/assets/images/BuluBaria-card.png";
-
+import dataGunung from '@/data/dataGunungDummy.json'
 function CardListGunung() {
   return (
     <>
@@ -102,15 +102,16 @@ function CardListGunung() {
         </div>
         <div className="pt-6 flex flex-wrap justify-between px-[20px] md:px-[50px] lg:px-[65px] ">
           {/* Card1 */}
+
           <div className="relative flex flex-wrap overflow-hidden bg-[#e7f2ff] hover:bg-[#d4e8ff] rounded-[30px] w-[685px] h-[338px] transition-all duration-500 ease-in-out">
-              <Image
-                src={BuluBaria}
-                alt="Bulu Baria"
-                width={0}
-                height={0}
-                className="object-cover  h-full w-full"
-              />
-            <div className="absolute inset-0 bg-black/45 "></div>     
+            <Image
+              src={BuluBaria}
+              alt="Bulu Baria"
+              width={0}
+              height={0}
+              className="object-cover  h-full w-full"
+            />
+            <div className="absolute inset-0 bg-black/45 "></div>
             <div>
               <div className="absolute inset-5 text-[65px] mx-5 font-semibold text-white">
                 Bulu Baria
@@ -132,14 +133,14 @@ function CardListGunung() {
           </div>
           {/* Card2 */}
           <div className="relative flex flex-wrap overflow-hidden bg-[#e7f2ff] hover:bg-[#d4e8ff] rounded-[30px] w-[685px] h-[338px] transition-all duration-500 ease-in-out">
-              <Image
-                src={BuluBaria}
-                alt="Bulu Baria"
-                width={0}
-                height={0}
-                className="object-cover  h-full w-full"
-              />
-            <div className="absolute inset-0 bg-black/45 "></div>     
+            <Image
+              src={BuluBaria}
+              alt="Bulu Baria"
+              width={0}
+              height={0}
+              className="object-cover  h-full w-full"
+            />
+            <div className="absolute inset-0 bg-black/45 "></div>
             <div>
               <div className="absolute inset-5 text-[65px] mx-5 font-semibold text-white">
                 Bulu Baria
@@ -162,92 +163,40 @@ function CardListGunung() {
         </div>
         <div className="pt-[65px] flex flex-wrap justify-between px-[20px] md:px-[50px] lg:px-[65px] ">
           {/* Card1 */}
-          <div className="relative flex flex-wrap overflow-hidden bg-[#e7f2ff] hover:bg-[#d4e8ff] rounded-[25px] w-[452px] h-[240px] transition-all duration-500 ease-in-out mb-5">
-              <Image
-                src={BuluBaria}
-                alt="Bulu Baria"
-                width={0}
-                height={0}
-                className="object-cover  h-full w-full"
-              />
-            <div className="absolute inset-0 bg-black/45 "></div>     
-            <div>
-              <div className="absolute inset-5 text-[50px] -mt-4 font-semibold text-white">
-                Bulu Baria
-                <h2 className="text-white text-lg -mt-2 max-w-md ">
-                  2750 MDPL
-                </h2>
-                <div className="grid grid-cols-2">
-                  <h2 className="text-[20px] font-normal mt-20">
-                    3 Trip Available
+          {dataGunung.map((mountain) => (
+            <div key={mountain.id} className="relative flex flex-wrap overflow-hidden bg-[#e7f2ff] hover:bg-[#d4e8ff] rounded-[25px] w-[452px] h-[240px] transition-all duration-500 ease-in-out mb-5">
+              <div className="relative w-full h-full">
+                <Image
+                  src={mountain.gambar}
+                  alt={mountain.nama_gunung}
+                  layout="fill" // Make the image fill the container
+                  objectFit="cover" // Ensure the image covers the entire container without stretching
+                  className="transition-all duration-300 rounded-2xl"
+                />
+                <div className="absolute inset-0 bg-black/45"></div>
+              </div>
+              <div>
+                <div className="absolute inset-5 text-[50px] -mt-4 font-semibold text-white">
+                  {mountain.nama_gunung}
+                  <h2 className="text-white text-lg -mt-2 max-w-md">
+                    {mountain.ketinggian}
                   </h2>
-                  <button className="w-[145px] bg-white h-[40px] text-lg my-3 mx-12 mt-20  flex items-center justify-center rounded-full cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md scale-105 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-[#4A90E2] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0 group">
-                    <span className="z-10 transition-colors duration-500 text-black group-hover:text-white">
-                      Lihat
-                    </span>
-                  </button>
+                  <div className="grid grid-cols-2">
+                    <h2 className="text-[20px] font-normal mt-20">
+                      {mountain.trip_tersedia}
+                    </h2>
+                    <button className="w-[145px] bg-white h-[40px] text-lg my-3 mx-12 mt-20 flex items-center justify-center rounded-full cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md scale-105 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-[#4A90E2] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0 group">
+                      <span className="z-10 transition-colors duration-500 text-black group-hover:text-white">
+                        Lihat
+                      </span>
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          {/* Card2 */}
-          <div className="relative flex flex-wrap overflow-hidden bg-[#e7f2ff] hover:bg-[#d4e8ff] rounded-[25px] w-[452px] h-[240px] transition-all duration-500 ease-in-out mb-5">
-              <Image
-                src={BuluBaria}
-                alt="Bulu Baria"
-                width={0}
-                height={0}
-                className="object-cover  h-full w-full"
-              />
-            <div className="absolute inset-0 bg-black/45 "></div>     
-            <div>
-              <div className="absolute inset-5 text-[50px] -mt-4 font-semibold text-white">
-                Bulu Baria
-                <h2 className="text-white text-lg -mt-2 max-w-md ">
-                  2750 MDPL
-                </h2>
-                <div className="grid grid-cols-2">
-                  <h2 className="text-[20px] font-normal mt-20">
-                    3 Trip Available
-                  </h2>
-                  <button className="w-[145px] bg-white h-[40px] text-lg my-3 mx-12 mt-20  flex items-center justify-center rounded-full cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md scale-105 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-[#4A90E2] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0 group">
-                    <span className="z-10 transition-colors duration-500 text-black group-hover:text-white">
-                      Lihat
-                    </span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* Card3 */}
-          <div className="relative flex flex-wrap overflow-hidden bg-[#e7f2ff] hover:bg-[#d4e8ff] rounded-[25px] w-[452px] h-[240px] transition-all duration-500 ease-in-out mb-5">
-              <Image
-                src={BuluBaria}
-                alt="Bulu Baria"
-                width={0}
-                height={0}
-                className="object-cover  h-full w-full"
-              />
-            <div className="absolute inset-0 bg-black/45 "></div>     
-            <div>
-              <div className="absolute inset-5 text-[50px] -mt-4 font-semibold text-white">
-                Bulu Baria
-                <h2 className="text-white text-lg -mt-2 max-w-md ">
-                  2750 MDPL
-                </h2>
-                <div className="grid grid-cols-2">
-                  <h2 className="text-[20px] font-normal mt-20">
-                    3 Trip Available
-                  </h2>
-                  <button className="w-[145px] bg-white h-[40px] text-lg my-3 mx-12 mt-20  flex items-center justify-center rounded-full cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md scale-105 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-[#4A90E2] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0 group">
-                    <span className="z-10 transition-colors duration-500 text-black group-hover:text-white">
-                      Lihat
-                    </span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
+
+
 
         </div>
       </div>
