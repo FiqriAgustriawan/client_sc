26df822bc8957468679485c0ada5a608df5aa569
DanielCaturrangga <muh.caturrangga@gmail.com>
2025-02-03 13:24:16 +0800

    liat mi

--- src/app/(dashboard-admin)/admin/page.tsx
+++ src/app/(dashboard-admin)/admin/page.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
-      
-    </div>
+    <>
+     <div className="min-h-screen p-4 md:p-10 flex flex-col md:flex-row mt-16 md:mr-5 max-w-[1200px] mx-auto">
+        <div className="hidden md:block w-[10%]">{/* sidebar space */}</div>
+       
+    </div> 
+    </>
   )
 }
 
