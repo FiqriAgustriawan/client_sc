2c94a7004ad516a31c2e3894be70858eec65bad4
adslfarass_ <adisalfaras08@icloud.com>
2025-02-10 08:19:47 +0800

    aassdasd

--- src/app/(dashboard-admin)/admin/kelolah-berita/page.tsx
+++ src/app/(dashboard-admin)/admin/kelolah-berita/page.tsx
@@ -4,6 +4,7 @@ import bg from "@/assets/images/FotoBerita.png";
 import Link from "next/link";
 
 export default function HistoryTrip() {
+  
   return (
     <div className="bg-white p-4 sm:p-6 w-full max-w-[95%] sm:max-w-[90%] md:max-w-[80%] lg:max-w-[70%] xl:max-w-[66%] xl:mx-[29%] mt-8 sm:mt-12 md:mt-16 md:mx-[15%] sm:mx-[5%] mx-5 lg:mt-20 lg:mx-[35%] shadow-lg rounded-3xl">
 
